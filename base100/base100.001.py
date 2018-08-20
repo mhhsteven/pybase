a6ec04c1fc9d571bd2f2e8c4c414e9807aa2a87b
mhh <mhh_steven@sina.com>
2018-08-20 22:12:44 +0800

    阶段提交

--- base100/base100.001.py
+++ base100/base100.001.py
@@ -3,14 +3,12 @@
 
 题目：有1、2、3、4个数字，能组成多少个互不相同且无重复数字的三位数？都是多少？
 '''
-
-
 def assemblyList(maxNum):
     for x in range(1, maxNum):
         for y in range(1, maxNum):
             for z in range(1, maxNum):
                 if x != y and x != z and y != z:
-                    print(x * 100 + y * 10 + z)
-
-
-assemblyList(5)
+                    yield x * 100 + y * 10 + z
+numList = list(assemblyList(5))
+print(numList)
+print('总计' + str(len(numList)) + '个')
